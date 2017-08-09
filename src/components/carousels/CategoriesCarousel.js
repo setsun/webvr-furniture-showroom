68358a12131fb122d0f8a973b25cc2a91655dec2
Sean Je Luo <seanjeluo@gmail.com>
2017-08-09 18:18:24 -0400

    basic category carousel

--- src/components/carousels/CategoriesCarousel.js
+++ src/components/carousels/CategoriesCarousel.js
@@ -16,11 +16,23 @@ class CategoriesCarousel extends React.Component {
       <Entity position="0 1.5 0" rotation="-90 0 0">
         <a-cylinder
           rotation="90 0 0"
-          height="1.6"
+          height="2.5"
+          material="side: double; transparent:true; opacity: 0.5;"
           open-ended="true"
           color="gray"
+          radius="5"
         />
         <a-entity layout="type: circle; margin: 6; radius: 3.25">
+          <a-entity obj-model="obj: #tree-obj; mtl: #tree-mtl" rotation="90 0 0" scale="0.5 0.5 0.5"/>
+          <a-entity obj-model="obj: #tree-obj; mtl: #tree-mtl" rotation="90 0 0" scale="0.5 0.5 0.5"/>
+          <a-entity obj-model="obj: #tree-obj; mtl: #tree-mtl" rotation="90 0 0" scale="0.5 0.5 0.5"/>
+          <a-entity obj-model="obj: #tree-obj; mtl: #tree-mtl" rotation="90 0 0" scale="0.5 0.5 0.5"/>
+          <a-entity obj-model="obj: #tree-obj; mtl: #tree-mtl" rotation="90 0 0" scale="0.5 0.5 0.5"/>
+          <a-entity obj-model="obj: #tree-obj; mtl: #tree-mtl" rotation="90 0 0" scale="0.5 0.5 0.5"/>
+          <a-entity obj-model="obj: #tree-obj; mtl: #tree-mtl" rotation="90 0 0" scale="0.5 0.5 0.5"/>
+          <a-entity obj-model="obj: #tree-obj; mtl: #tree-mtl" rotation="90 0 0" scale="0.5 0.5 0.5"/>
+          <a-entity obj-model="obj: #tree-obj; mtl: #tree-mtl" rotation="90 0 0" scale="0.5 0.5 0.5"/>
+          <a-entity obj-model="obj: #tree-obj; mtl: #tree-mtl" rotation="90 0 0" scale="0.5 0.5 0.5"/>
         </a-entity>
       </Entity>
     );
