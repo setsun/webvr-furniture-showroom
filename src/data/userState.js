3277fb6a445fada86dc5d01984942280201407e3
Sean Je Luo <setsun@setsun.io>
2017-08-09 13:30:28 -0400

    git them tables in

--- src/data/userState.js
+++ src/data/userState.js
@@ -1,7 +1,20 @@
 const initialState = [];
 
+export const ActionTypes = {
+  BUTTON_CLICKED: 'BUTTON_CLICKED',
+};
+
+export const onButtonClicked = id => ({
+  type: ActionTypes.BUTTON_CLICKED,
+  payload: { id },
+});
+
 export default (state = initialState, action) => {
   switch (action.type) {
+    case ActionTypes.BUTTON_CLICKED: {
+      console.log('FIRED');
+      return state.concat(Math.random());
+    }
     default:
       return state;
   }
