5ae22706d5abc36a3e4aa12f01f9882528ff8c42
Sean Je Luo <seanjeluo@gmail.com>
2017-08-09 14:43:41 -0400

    boilterplate for components

---
+++ src/components/carousel/CategoriesCarousel.js
@@ -0,0 +1,17 @@
+import 'aframe-animation-component';
+import {component} from 'aframe-layout';
+
+import React from 'react';
+import {Entity} from 'aframe-react';
+
+class CategoriesCarousel extends React.Component {
+  render() {
+    return (
+      <Entity>
+
+      </Entity>
+    );
+  }
+}
+
+export default CategoriesCarousel;
