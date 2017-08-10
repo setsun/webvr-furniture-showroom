fd111c007e057135928edf58c8e9dbf8b599e66b
Sean Je Luo <setsun@setsun.io>
2017-08-10 12:54:18 -0400

    assets

--- src/components/product/AddToCartButton.js
+++ src/components/product/AddToCartButton.js
@@ -20,6 +20,11 @@ class AddToCartButton extends React.Component {
           height="0.2"
           width="0.2"
         />
+        <a-ring
+          material="transparent: true; opacity: 0.75;"
+          radius-inner="0.1"
+          radius-outer="0.125"
+        />
       </a-entity>
     );
   }
