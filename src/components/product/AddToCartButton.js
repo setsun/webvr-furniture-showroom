42429c9f21595ae3cb9bd630588d6e97e4977009
Sean Je Luo <setsun@setsun.io>
2017-08-10 01:37:11 -0400

    moar boilterplate for loading

--- src/components/product/AddToCartButton.js
+++ src/components/product/AddToCartButton.js
@@ -17,7 +17,7 @@ class AddToCartButton extends React.Component {
       <a-entity onClick={() => onAddToCart()}>
         {false && <a-image src="#icon-add-to-cart" />}
         <a-circle
-          radius="0.1"
+          radius="0.11"
           color="#8200FF"
         />
       </a-entity>
